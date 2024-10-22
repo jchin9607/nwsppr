c43cc18af780c7dc67639cc9984d4f9bf70be156
Dhruva S <105791976+dhrksuri@users.noreply.github.com>
2024-10-22 14:23:40 -0400

    Update Fourohfour.jsx
    never mind lucas

--- src/pages/Fourohfour.jsx
+++ src/pages/Fourohfour.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import robotImage from './JAKSON-IS-A-robot-image.png'; //LUCAS PUT A ROBOT PICTURE SOMEWHERE HERE
+ //LUCAS forget the robot
 
 const Fourohfour = () => {
   return (
     <div className="flex flex-col items-center justify-center h-[90vh] w-full">
       
-      <img src={robotImage} alt="Robot" className="mb-4 w-32 h-32" />
+      
 
       
-      <div className="text-6xl font-bold text-center" style={{ fontFamily: 'Martian Mono, monospace' }}>
+      <div className="text-black-500 text-6xl font-bold text-center" style={{ fontFamily: 'Martian Mono, monospace' }}>
         404: Page/Search Not Found
       </div>
+       <div className="text-grey-325 text-4xl font-bold- text-center"  style={{ fontFamily: 'Martian Mono, monospace' }}>
+        writeup
     </div>
   );
 };
